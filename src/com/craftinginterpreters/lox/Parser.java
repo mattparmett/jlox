485f489337186ebcffef3fa0b0d7c7e9792c1a54
Matt Parmett <1214074+mattparmett@users.noreply.github.com>
2024-02-07 17:23:33 -0800

    Add block syntax.

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -85,6 +85,7 @@ class Parser {
    */
   private Stmt statement() {
     if (match(PRINT)) return printStatement();
+    if (match(LEFT_BRACE)) return new Stmt.Block(block());
     return expressionStatement();
   }
 
@@ -99,6 +100,23 @@ class Parser {
     Expr value = expression();
     consume(SEMICOLON, "Expect ';' after value.");
     return new Stmt.Print(value);
+  }
+
+  /*
+   * Parses a block as a list of statements
+   * (declarations) within curly braces.
+   * 
+   * Returns a list of statements within the block.
+   */
+  private List<Stmt> block() {
+    List<Stmt> statements = new ArrayList<>();
+
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      statements.add(declaration());
+    }
+
+    consume(RIGHT_BRACE, "Expect '}' after block.");
+    return statements;
   }
 
   /*
