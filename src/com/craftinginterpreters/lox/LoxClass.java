38c94eb73ad24d5dcc0ce1ece141d7887700df86
Matt Parmett <1214074+mattparmett@users.noreply.github.com>
2024-02-16 22:02:52 -0800

    Add support for rudimentary class instances.

--- src/com/craftinginterpreters/lox/LoxClass.java
+++ src/com/craftinginterpreters/lox/LoxClass.java
@@ -3,11 +3,32 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 import java.util.Map;
 
-public class LoxClass {
+public class LoxClass implements LoxCallable {
     final String name;
 
     LoxClass(String name) {
         this.name = name;
+    }
+
+    /*
+     * Tells the interpreter how many arguments
+     * to expect in the class constructor.
+     * 
+     * TODO: make dynamic for user-defined constructors
+     */
+    @Override
+    public int arity() {
+        return 0;
+    }
+
+    /*
+     * Calling a class directly instantiates a new
+     * class instance, and returns it.
+     */
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
     }
 
     @Override
